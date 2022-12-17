a1828c2666f6312143ef33ac80e28237f1be62a6
Don Wonders <wonders.don@gmail.com>
2022-12-17 15:34:44 -0500

    add support for alternate dns names

--- bare-metal/fedora-coreos/kubernetes/bootstrap.tf
+++ bare-metal/fedora-coreos/kubernetes/bootstrap.tf
@@ -3,7 +3,7 @@ module "bootstrap" {
   source = "git::https://github.com/poseidon/terraform-render-bootstrap.git?ref=9af5837c35411939111dff1b00e52faf26b179a2"
 
   cluster_name           = var.cluster_name
-  api_servers            = [var.k8s_domain_name]
+  api_servers            = concat([var.k8s_domain_name], var.k8s_alt_domain_names)
   service_account_issuer = var.service_account_issuer
   etcd_servers           = var.controllers.*.domain
   networking             = var.networking
